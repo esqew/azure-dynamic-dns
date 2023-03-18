d9571207b6026d3aa91e7aa3c8e5a3d7bf081b5d
Sean F Quinn <sean@esqew.com>
2023-03-18 00:06:14 -0400

    Added ability to change providers configuration

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM node:18-alpine3.17
 ENTRYPOINT [ "node" ]
 WORKDIR /usr/src/azure-dynamic-dns
 COPY package*.json ./
+RUN npm update -g npm
 RUN npm install
 COPY . .
 CMD [ "dns-daemon.js", "--trace-uncaught" ]
--- dns-daemon.js
+++ dns-daemon.js
@@ -8,6 +8,9 @@
  * See LICENSE.md for licensing information
  */
 
+// Require project-specific class definitions from ./lib
+const ProviderArray = require('./lib/ProviderArray');
+
 // Require official Azure libraries to for abstracted API calls
 const { DnsManagementClient } = require("@azure/arm-dns");
 const { ClientSecretCredential } = require("@azure/identity");
@@ -15,33 +18,7 @@ const { ClientSecretCredential } = require("@azure/identity");
 // Require node-cron for daemonized operation at interval configured in .env
 const cron = require('node-cron');
 
-// Require built-in https module to facilitate communication with WAN IP echo service without adding an additional dependency
-const https = require('https');
-
-/**
- * Retrieves the WAN IP from the echo service
- * @async
- */
-const getWANIP = async () => {
-    return new Promise((resolve, reject) => {
-        var req = https.get(`https://icanhazip.com/`, response => {
-            if (response.statusCode < 200 || response.statusCode >= 300) {
-                return reject(new Error(`Status Code: ${response.statusCode}`));
-            }
-
-            var data = [];
-            
-            response.on('data', chunk => {
-                data.push(chunk);
-            });
-            
-            response.on('end', () => resolve(Buffer.concat(data).toString().trim()));
-        });
-        req.on('error', reject);
-        
-        req.end();
-    });
-}
+var providers = ProviderArray.fromProvidersArray(require('./config/providers.json'));
 
 /**
  * Checks if the update is necessary and, if so, updates the Azure DNS record per the configuration in .env
@@ -57,18 +34,20 @@ const do_update = async (dry_run = false) => {
         client.recordSets.get(
             process.env.RESOURCE_GROUP_NAME,
             process.env.ZONE_NAME,
-            process.env.RELATIVE_RECORD_SET_NAME,
+            process.env.RELATIVE_RECORD_SET,
             process.env.RECORD_TYPE
-        ).then(result => result.aRecords.map(obj => obj.ipv4Address) || []),
+        ).then(
+            result => result[process.env.RECORD_TYPE.toLowerCase() + "Records"].map(obj => obj.ipv4Address) || []
+        ).catch(err => {
+            console.error(err);
+        }),
         // Get the WAN IP of the current network by calling to an external IP echo service
-        getWANIP()
+        providers.getRandomProvider().getResult()
     ]).then(result => {
         // Evaluate & take action based on the data returned from both Promises above
-
-        // Normally, we can reduce the verbosity of this equality check (which needs to guard against result[0] being zero-length or undefined) by using optional chaining (.?) available in Node v14.x and later.
-        // It has instead been designed with two distinct expressions to prevent exclusion of earlier versions of Node unnecessarily
-        if (result[0].length > 0 && !result[0].includes(result[1])) {
-            if (dry_run) console.log(`[DRY RUN] Azure DNS ${process.env.RECORD_TYPE} record ${process.env.RELATIVE_RECORD_SET_NAME}.${process.env.ZONE_NAME} would have been updated from ${result[0]} to ${result[1]}`)
+        if (!result[0]?.includes(result[1])) {
+            // Current record does not include the currently-detected WAN IP
+            if (dry_run) console.log(`[DRY RUN] Azure DNS ${process.env.RECORD_TYPE} record ${process.env.RELATIVE_RECORD_SET}.${process.env.ZONE_NAME} would have been updated from ${result[0]} to ${result[1]}`)
             else {
                 client.recordSets.update(
                     process.env.RESOURCE_GROUP_NAME,
@@ -78,7 +57,7 @@ const do_update = async (dry_run = false) => {
                     {
                         aRecords: [{ ipv4Address: result[1] }]
                     }
-                ).then(() => console.log(`Updated current WAN IP (${result[1]}) to Azure DNS ${process.env.RECORD_TYPE} record ${process.env.RELATIVE_RECORD_SET_NAME}.${process.env.ZONE_NAME}`));
+                ).then(() => console.log(`Updated current WAN IP (${result[1]}) to Azure DNS ${process.env.RECORD_TYPE} record ${process.env.RELATIVE_RECORD_SET}.${process.env.ZONE_NAME}`));
             }
         }
         else console.log(`${dry_run ? '[DRY RUN] ' : ''}No update to DNS record necessary - target record value matches current WAN IP (${result[1]})`);
